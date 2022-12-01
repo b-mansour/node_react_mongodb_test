0153ab31f03c618447c331273cda92e0d149acb5
bashermansour <basher@dafa.sa>
2022-12-01 14:00:54 +0300

    started working on react-beautiful dnd draggable

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,47 +1,104 @@
 import React, { useState } from "react";
-import { DragDropContext, Dropappable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "./Home.css";
 
 const data = [
   {
-    id: "list-1",
+    id: "1",
     title: "To do",
-    cards: [
+    items: [
       {
-        id: "1",
-        title: "vim practice",
+        id: "764378974389",
+        title: "title",
+        content: "content",
       },
     ],
   },
   {
-    id: "list-2",
+    id: "2",
     title: "In Progress",
-    cards: [],
+    items: [
+      {
+        id: "298734789387",
+        title: "title",
+        content: "content",
+      },
+    ],
   },
   {
-    id: "list-3",
+    id: "3",
     title: "Completed",
-    cards: [],
+    items: [
+      {
+        id: "3887438973498",
+        title: "title",
+        content: "content",
+      },
+    ],
   },
 ];
 
-const backendcolumns = ["list-1", "list-2", "list-3"];
-
 export default function Home() {
-  const [columns, setCoumns] = useState(backendcolumns);
+  const [columns, setCoumns] = useState(data);
   return (
     <div className="container">
-      {columns.map((column) => {
-        console.log(column);
-        data.map((item) => {
-          if (item.id === column) {
-            console.log(item);
-          }
+      {/* {columns.map((column) => {
+        console.log(column.title);
+        column.items.map((item) => {
+          console.log(item.content);
         });
-      })}
-      {/* <DragDropContext
-        onDropEnd={(result) => console.log(result)}
-      ></DragDropContext> */}
+      })} */}
+      <DragDropContext onDropEnd={(result) => console.log(result)}>
+        {columns.map((column) => {
+          return (
+            <Droppable key={column.id} droppableId={column.id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    className="droppable"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
+                        ? "#b4b8b7"
+                        : "#737877",
+                    }}
+                  >
+                    {column.items.map((item, index) => {
+                      return (
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}
+                        >
+                          {(provided, snapshot) => {
+                            return (
+                              <div
+                                className="draggable"
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                style={{
+                                  background: snapshot.isDragging
+                                    ? "yellow"
+                                    : "blue",
+                                  ...provided.draggableProps.style,
+                                }}
+                              >
+                                {item.content}
+                              </div>
+                            );
+                          }}
+                        </Draggable>
+                      );
+                    })}
+                  </div>
+                );
+              }}
+            </Droppable>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
